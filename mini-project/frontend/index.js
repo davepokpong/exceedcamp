a0d76412309f604c838af62759fc8c85a30fb7b2
M CPE <mmmiko@hotmail.com>
2021-02-13 13:20:34 +0700

    wow

--- mini-project/frontend/index.js
+++ mini-project/frontend/index.js
@@ -1,2 +1,14 @@
 app = document.getElementById("app");
+const cache = []
 
+setInterval(() => {
+    fetch("158.108.182.14:50004/check")
+    .then(data => data.json())
+    .then(datas => {
+        datas.forEach(data => {
+            if(datas){
+                
+            }
+        });
+    });
+}, 5000);
