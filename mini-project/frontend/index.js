a13fb8def8f6360c86b44bcd399612f31c8950ce
M CPE <mmmiko@hotmail.com>
2021-02-13 15:47:32 +0700

    wow

--- mini-project/frontend/index.js
+++ mini-project/frontend/index.js
@@ -1,12 +1,12 @@
 app = document.getElementById("app");
 const cache2 = [{
     "slot": 1,
-    "state": 1,
+    "state": 0,
     "time": 0
 },
 {
     "slot": 2,
-    "state": 0,
+    "state": 1,
     "time": 0
 },
 {
@@ -18,29 +18,30 @@ const cache2 = [{
     "slot": 4,
     "state": 0,
     "time": 0
-},
-{
-    "slot": 5,
-    "state": 1,
-    "time": "Sat, 13 Feb 2021 06:41:21 GMT"
 }]
 
 const cache = []
 var link = document.createElement('index.css');
 
-rec1 = document.getElementById("rec1");
-rec2 = document.getElementById("rec2")
+// var rec1 = document.getElementsByID("rec").s;
+// var rec2 = document.getElementsByID("rec");
 
-// fetch("https://exceed12.cpsk-club.xyz/", {
-//     method: "GET",
-//     headers: { "Content-Type": "application/json" },
-// })
-// .then(data => data.json())
-// .then(data => console.log(data))
+fetch("https://exceed12.cpsk-club.xyz", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+})
+.then(data => data.json())
+.then(data => console.log(data))
 
 cache2.forEach(x =>{
-    if(x.state == '0'){
-        console.log(x.slot)
+    if(x.state == '1'){
+        console.log(x.slot);
         // $('rec1').css('background-color','green')}
-        
+        document.getElementById(x.slot).style.backgroundColor="#EE4A52"
+    }    
+    if(x.state == '0'){
+        console.log(x.slot);
+        // $('rec1').css('background-color','green')}
+        document.getElementById(x.slot).style.backgroundColor="#57C9AE"
+    }
 })
